2f424f556f221dc166316ea951ea7583f264de75
DPlayer234 <dplayer.235@gmail.com>
2017-11-06 18:56:07 +0100

    Logo?

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -15,24 +15,28 @@ namespace SAE.YA3DT
         /// <summary> How long it takes until the block is completely removed after being broken </summary>
         public float timeUntilRemoved = 1.0f;
 
-        /// <summary>
-        ///     Returns a random value to be used as a force added within <seealso cref="BreakMe"/>
-        /// </summary>
-        /// <returns>A float in the range of [-<seealso cref="FORCE_VALUE_RANGE"/>, <seealso cref="FORCE_VALUE_RANGE"/>]</returns>
-        private float GetRandomForce()
-        {
-            return (Random.value - 0.5f) * 2 * FORCE_VALUE_RANGE;
-        }
+		/// <summary> This block's box collider </summary>
+		private BoxCollider boxCollider;
+
+		/// <summary> This block's rigidbody </summary>
+		private Rigidbody rigidbody;
+
+		/// <summary>
+		/// 	Called by Unity to initialize the Block
+		/// </summary>
+		private void Start()
+		{
+            boxCollider = GetComponent<BoxCollider>();
+            rigidbody = GetComponent<Rigidbody>();
+		}
 
         /// <summary>
         ///     Called when the Block is supposed to be removed from the PlayField
         /// </summary>
         public void BreakMe()
         {
-            BoxCollider boxCollider = GetComponent<BoxCollider>();
             boxCollider.isTrigger = false;
 
-            Rigidbody rigidbody = GetComponent<Rigidbody>();
             rigidbody.AddForce(GetRandomForce(), GetRandomForce(), GetRandomForce());
             rigidbody.useGravity = true;
         }
@@ -46,6 +50,15 @@ namespace SAE.YA3DT
         {
             timeUntilRemoved -= Time.deltaTime;
             return timeUntilRemoved < 0f;
+        }
+
+        /// <summary>
+        ///     Returns a random value to be used as a force added within <seealso cref="BreakMe"/>
+        /// </summary>
+        /// <returns>A float in the range of [-<seealso cref="FORCE_VALUE_RANGE"/>, <seealso cref="FORCE_VALUE_RANGE"/>]</returns>
+        private float GetRandomForce()
+        {
+            return (Random.value - 0.5f) * 2 * FORCE_VALUE_RANGE;
         }
     }
 }
